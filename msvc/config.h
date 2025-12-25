a90302c46d0300507984759984bb81956aebc311
ogamespec <ogamespec@gmail.com>
2025-12-25 10:11:30 +0300

    getopt.c

--- msvc/config.h
+++ msvc/config.h
@@ -7,3 +7,13 @@ typedef SSIZE_T ssize_t;
 
 #define HAVE_STRNLEN
 #define HAVE_GETTIMEOFDAY
+#define IS_WIN32 1
+#define _TIMEVAL_DEFINED
+
+#define VERSION "0.12"
+#ifdef _DEBUG
+#define RELSTR " Debug"
+#else
+#define RELSTR " Release"
+#endif
+#define GITVERSION "0.12"
